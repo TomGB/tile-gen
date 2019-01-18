1c8c686da6ca517bef99ecb02f9a3aa8cd262e3a
Thomas Banister <tgbanister@gmail.com>
2019-01-18 17:31:06 +0000

    added query string

--- src/browser/script.js
+++ src/browser/script.js
@@ -1,9 +1,20 @@
+const queryString = require('query-string');
 const generateCanvas = require('../utils/generateCanvas');
-
 const twoGradients = require('../patterns/twoGradients');
+const gradEveryLine = require('../patterns/gradEveryLine');
+ 
+const runPattern = pattern => {
+    if (pattern === 'gradEveryLine') {
+        return gradEveryLine();
+    }
+
+    return twoGradients();
+}
 
 const newImage = async () => {
-    const colourMap = twoGradients();
+    const { pattern } = queryString.parse(location.search);
+
+    const colourMap = runPattern(pattern);
     canvas = await generateCanvas(colourMap);
 
     const imageArea = document.getElementById("image-area");
